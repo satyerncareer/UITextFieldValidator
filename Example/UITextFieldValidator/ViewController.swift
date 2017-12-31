fd5b3c5d08fc2dcef3b4b16b2903e7b37729ec59
satyendra chauhan <satyendra.chauhan@appster.in>
2017-12-31 12:35:22 +0530

    Initial commit

---
+++ Example/UITextFieldValidator/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UITextFieldValidator
+//
+//  Created by satyencareer@gmail.com on 12/31/2017.
+//  Copyright (c) 2017 satyencareer@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
